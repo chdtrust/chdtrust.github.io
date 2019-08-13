6a3d00f2ce5594765d3382e09473410ddfdaeb5b
chdtrust <54061927+chdtrust@users.noreply.github.com>
2019-08-13 15:33:09 +0530

    Delete index.php

--- index.php
+++
@@ -1,4 +0,0 @@
-Welcome 
-To 
-@chdtrust
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfCaMkmOj3Jjzzpuge-M3cPzPLSOqXT5cnHFf35CzKX9FQsQw/viewform?embedded=true" width="640" height="1288" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
